7e0618dd72e5b2651867161e34b671283c20816c
Miroslav Parapanov <mparapanov99@gmail.com>
2020-10-30 17:13:30 +0200

    set-intersec done

--- Homework1/2.rkt
+++ Homework1/2.rkt
@@ -1,4 +1,6 @@
 ;;Помощни функции:
+(define (get-last-digit n) (remainder n 10))
+
 
 (define (toBinary n)
   (define (helper n res bit)
@@ -29,11 +31,11 @@
   (helper (toBinary n) 0))
 
 (define (set-add set elem)
-  (if (contains? set elem) set
+  (if (or (contains? set elem) (negative? elem)) set
       (+ set (expt 2 elem))))
 
 (define (set-remove set elem)
-  (if (not (contains? set elem))set
+  (if (or (not (contains? set elem)) (negative? elem))set
       (- set (expt 2 elem))))
 
 ;;Глупав начин за дефинираме на set-add :
@@ -45,6 +47,7 @@
 ;          ((= elem bit) res)
 ;          (else (helper (quotient newSet 2) (+ res (* (remainder newSet 2) (expt 10 bit))) (+ bit 1)))))
 ;    (toDecimal (helper set 0 0)))
+
 (define (set-empty? set)
   (if (= 0 set)#t #f))
 
@@ -55,6 +58,11 @@
           (else (helper (quotient newset 10) count))))
   (helper (toBinary set) 0))
 
-
-
-  
+(define (set-intersec s1 s2)
+  (define (helper newSet1 newSet2 res bit)
+    (cond ((or (= newSet1 0) (= newSet2 0)) (toDecimal res))
+          ((and (= 1 (get-last-digit newSet1)) (= 1 (get-last-digit newSet2))) (helper (quotient newSet1 10) (quotient newSet2 10) (+ res (* 1 (expt 10 bit))) (+ bit 1)))
+          (else (helper (quotient newSet1 10) (quotient newSet2 10) (+ res (* 0 (expt 10 bit))) (+ bit 1)))))
+  (helper (toBinary s1) (toBinary s2) 0 0))
+          
+(set-intersec 35 23)
