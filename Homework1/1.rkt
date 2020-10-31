5539d8a47434c45638817cae0fc0b99111bac22a
Miroslav Parapanov <mparapanov99@gmail.com>
2020-10-31 19:17:04 +0200

    Accumulate lectures and starting again

--- Homework1/1.rkt
+++ Homework1/1.rkt
@@ -5,36 +5,22 @@
 ;; down-left-corner -->  \u2514
 ;; down-right-corner --> \u2518
 
-;; Понеже хоризонталните линии са повече от вертикалните
-;; и от пример се вижда, че при 3 квадрата има 9 хоризонтални
-;; ------------- // ----------- 2 квадрата има 5 и т.н
-;; (тоест или се увеличават с 4, или се намалят с 4 в заисимост от това колко са квадратите
+;; Accumulate:
+
+(define (accumulate op term init a next b)  
+  (define (loop i)
+      (if (<= i b)
+          (op (term i) (loop (next i)) )
+          init
+  ))
+  (loop a)
+)
+
 (define (how-many-squares-horziontals n)
   (cond ((= n 1 ) 1)
         (else (+ 4 (how-many-squares-horziontals (- n 1))))))
 
-;; обяснението е същото, което следва от примера даден в Домашшно 1
 (define (how-many-squares-verticals n)
-  (cond ( (= n 1) 0)
+  (cond ((= n 1) 0)
         (else (+ 2 (how-many-squares-verticals (- n 1))))))
-;; рисува горната линия + двата ъгъла
-(define (squares-draw-up-line n)
-  (display "\u250C")
-  (display (make-string (how-many-squares-horziontals n) #\u2500))
-  (display "\u2510"))
-;; рисува долната линия + двата ъгъла
-(define (squares-draw-down-line n)
-  (display "\u2514")
-  (display (make-string (how-many-squares-horziontals n) #\u2500))
-  (display "\u2518"))
-
-
-(squares-draw-up-line 3)
-(display "\n")
-(squares-draw-down-line 3)
-
-
-
-
-
 
--- Homework1/2.rkt
+++ Homework1/2.rkt
@@ -1,6 +1,8 @@
 ;;Помощни функции:
 (define (get-last-digit n) (remainder n 10))
 
+(define (del-num-by-ten n) (quotient n 10))
+
 
 (define (toBinary n)
   (define (helper n res bit)
@@ -14,20 +16,20 @@
 (define (bits? n)
   (define (helper n count)
     (cond ((= n 0) count)
-          (else (helper (quotient n 10) (+ 1 count)))))
+          (else (helper (del-num-by-ten n) (+ 1 count)))))
   (helper (toBinary n) 0))
 
 
 (define (toDecimal n)
   (if (= n 0) 0
-      (+ (remainder n 10) (* 2 (toDecimal (quotient n 10))))))
+      (+ (del-num-by-ten n) (* 2 (toDecimal (quotient n 10))))))
 ;;Основни функции
 
 (define (contains? n elem)
   (define (helper n bits)
     (cond ((= n 0) #f)
           ((and (= bits elem) (= 1 (remainder n 2)))  #t)
-          (else (helper (quotient n 10) (+ 1 bits)))))
+          (else (helper (del-num-by-ten n) (+ 1 bits)))))
   (helper (toBinary n) 0))
 
 (define (set-add set elem)
@@ -54,15 +56,25 @@
 (define (set-size set)
   (define (helper newset count)
     (cond ((= newset 0) count)
-          ((= 1 (remainder newset 10)) (helper (quotient newset 10) (+ count 1)))
-          (else (helper (quotient newset 10) count))))
+          ((= 1 (remainder newset 10)) (helper (del-num-by-ten newset) (+ count 1)))
+          (else (helper (del-num-by-ten newset) count))))
   (helper (toBinary set) 0))
 
-(define (set-intersec s1 s2)
+(define (set-intersect s1 s2)
   (define (helper newSet1 newSet2 res bit)
     (cond ((or (= newSet1 0) (= newSet2 0)) (toDecimal res))
-          ((and (= 1 (get-last-digit newSet1)) (= 1 (get-last-digit newSet2))) (helper (quotient newSet1 10) (quotient newSet2 10) (+ res (* 1 (expt 10 bit))) (+ bit 1)))
-          (else (helper (quotient newSet1 10) (quotient newSet2 10) (+ res (* 0 (expt 10 bit))) (+ bit 1)))))
+          ((and (= 1 (get-last-digit newSet1)) (= 1 (get-last-digit newSet2))) (helper (del-num-by-ten newSet1) (del-num-by-ten newSet2) (+ res (* 1 (expt 10 bit))) (+ bit 1)))
+          (else (helper (del-num-by-ten newSet1) (del-num-by-ten newSet2) (+ res (* 0 (expt 10 bit))) (+ bit 1)))))
   (helper (toBinary s1) (toBinary s2) 0 0))
+;;Да се оправи:
+(define (set-union s1 s2)
+  (define (helper newSet1 newSet2 res bit)
+    (cond ((or (= newSet1 0) (= 0 newSet2)) (toDecimal res))
+          ((or (= 1 (get-last-digit newSet1)) (= 1 (get-last-digit newSet2))) (helper (del-num-by-ten newSet1) (del-num-by-ten newSet2) (+ res(* 1 (expt 10 bit))) (+ 1 bit)))
+          (else (helper (del-num-by-ten newSet1) (del-num-by-ten newSet2) (+ res(* 0 (expt 10 bit))) (+ 1 bit)))))
+  (helper (toBinary s1) (toBinary s2) 0 0))
+
+(set-union 35 23)
+
+
           
-(set-intersec 35 23)
