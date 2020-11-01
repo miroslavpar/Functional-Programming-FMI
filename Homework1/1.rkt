5e65ede7e1d39f86f08619680e0ff4370c63d4fa
Miroslav Parapanov <mparapanov99@gmail.com>
2020-11-01 20:17:28 +0200

    again

--- Homework1/1.rkt
+++ Homework1/1.rkt
@@ -4,47 +4,54 @@
 ;; up-right-corner -->   \u2510
 ;; down-left-corner -->  \u2514
 ;; down-right-corner --> \u2518
+;; space ---> \u0020
 
 ;;БЕЛЕЖКИ:
 
 ;; при n = 3 -> 9 хоризонтални и така през 4 в зависимост от n
 ;; при n = 3 -> 4 вертиканли и така през 2 в зависимост от n
 
-
 ;; Accumulate:
 
 (define (accumulate op term init a next b)  
   (define (loop i)
       (if (<= i b)
+          (op (term i) (loop (next i)))
+          init
+  ))
+  (loop a)
+)
+(define (accumulate-opp op term init a next b)  
+  (define (loop i)
+      (if (>= i b)
           (op (term i) (loop (next i)) )
           init
   ))
   (loop a)
 )
 
-(define (draw-up-line n fic)
+(define (draw-upper-body n _)
   (define (helper k)
-   (display (make-string (- n k) #\u0020))
-   (display "\u250C")
-   (display (make-string (- (* 4 k) 3) #\u2500))
-   (display "\u2510")
-   (display "\n"))
-  (helper n))
+  (if (> k 0) (begin   
+                (display (make-string (- n k) #\u2502))
+                (display (make-string (- n k) #\u0020))
+                (display "\u250C")
+                (display (make-string (- (* 4 k) 3) #\u2500))
+                (display "\u2510")
+                (display (make-string (- n k) #\u0020))
+                
+                (display "\n")
+                (helper (- k 1)))))
+ (helper n))
 
 (define (1+ x) (+ x 1))
 (define (1- x) (- x 1))
 (define (id x) x)
-(accumulate draw-up-line id 0 1 1+ 3)
+(accumulate draw-upper-body id 0 1 1+ 3)
 
 
 
-
-
-
-
-
-
-
+;(display (string  #\u0020 #\u2502))
 
 
 
